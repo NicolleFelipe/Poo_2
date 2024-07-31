9a5a791a7409876a2a8117dfe9b011f762aed660
NicolleFelipe <nfeliper1@miumg.edu.gt>
2024-07-30 23:33:51 -0600

    first commit

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author nicolle
+ */
+public class Cliente extends Persona {
+    private String nit;
+    public Cliente (){}
+    public Cliente(String nit, String nombres, String apellidos, String direccion, String telefono, String fecha_nacimiento) {
+        super(nombres, apellidos, direccion, telefono, fecha_nacimiento);
+        this.nit = nit;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+    @Override
+    public void agregar (){
+    System.out.println("Nit: "+getNit());
+    System.out.println("Nombres: "+getNombres());
+    System.out.println("Apellidos: "+getApellidos());
+    System.out.println("Direccion: "+getDireccion());
+    System.out.println("Telefono: "+getTelefono());
+    System.out.println("Fecha_nacimiento: "+getFecha_nacimiento());
+    System.out.println("______________________________________");
+    }
+}
